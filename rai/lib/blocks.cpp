fef5200b2b84db7c847dc701beff064ee0ebb4ee
adam <adam_rotaru@yahoo.com>
2019-06-12 23:12:06 +0200

    Comment block hash round 2, big endian everywhere.

--- rai/lib/blocks.cpp
+++ rai/lib/blocks.cpp
@@ -626,10 +626,10 @@ comment (comment_a)
 void rai::comment_hashables::hash (blake2b_state & hash_a) const
 {
 	uint32_t subtype_big_endian (subtype.number_big_endian ());
-	// include length explicitly
 	blake2b_update (&hash_a, &subtype_big_endian, sizeof (subtype_big_endian));
-	uint16_t comment_len (comment.length ());
-	blake2b_update (&hash_a, &comment_len, sizeof (comment_len));
+	// include length explicitly
+	uint16_t comment_len_big_endian (boost::endian::native_to_big (comment.length ()));
+	blake2b_update (&hash_a, &comment_len_big_endian, sizeof (comment_len_big_endian));
 	blake2b_update (&hash_a, comment.value ().data (), comment.value ().size ());
 }
 
